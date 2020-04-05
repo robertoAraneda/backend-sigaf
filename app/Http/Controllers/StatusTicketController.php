95187ddbb880a953c99f0da61c3b73ad00b48f0f
roberto <robaraneda@gmail.com>
2020-04-05 17:21:04 -0400

    se corrige parametros de store ticketStatus

--- app/Http/Controllers/StatusTicketController.php
+++ app/Http/Controllers/StatusTicketController.php
@@ -131,7 +131,7 @@ class StatusTicketController extends Controller
    * @param  int  $id
    * @return \Illuminate\Http\Response
    */
-  public function update(Request $request, $id)
+  public function update($id)
   {
 
     try {
