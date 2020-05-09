b9e9a7d61975c6ea4ae039e56a68b32fe15dc884
claudia_alarcon <c.alarconlazo@gmail.com>
2020-05-09 03:30:06 -0400

    refactorización completa de profile

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
@@ -10,17 +11,53 @@ class Profile extends Model
 
   protected $table = 'profiles';
 
+  /**
+   * Get the Profile formated
+   *
+   * @return array
+   */
   public function format()
   {
     return [
-      'id' => $this->id,
-      'description' => $this->description,
-      'createdAt' => $this->created_at,
-      'updatedAt' => $this->updated_at
+      'links' => [
+        'href' => route(
+          'api.profiles.show',
+          ['profile' => $this->id],
+          false
+        ),
+        'rel' => 'self'
+      ],
+      'properties' => [
+        'id' => $this->id,
+        'description' => $this->description,
+        'createdAt' => $this->created_at != null
+          ?  Carbon::parse($this->created_at)->format('d-m-Y')
+          : null,
+        'updatedAt' => $this->updated_at != null
+          ?  Carbon::parse($this->updated_at)->format('d-m-Y')
+          : null
+      ],
+      'nestedObjects' => [],
+      'relationships' => [
+        'numberOfElements' => $this->courseRegisteredUsers->count(),
+        'links' => [
+          'href' => route(
+            'api.profiles.courseRegisteredUsers',
+            ['profile' => $this->id],
+            false
+          ),
+          'rel' => '/rels/tickets'
+        ]
+      ]
     ];
   }
 
-    public function courseRegisteredUsers(){
-        return $this->hasMany(CourseRegisteredUser::class);
-    }
+  /**
+   * Get a list of course regostered users for the profile
+   *
+   */
+  public function courseRegisteredUsers()
+  {
+    return $this->hasMany(CourseRegisteredUser::class);
+  }
 }
--- routes/api.php
+++ routes/api.php
@@ -88,6 +88,7 @@ Route::group([
   Route::apiResource('/classrooms', 'ClassroomController')->names('api.classrooms');
   Route::apiResource('/roles', 'RoleController')->names('api.roles');
   Route::apiResource('/source-tickets', 'SourceTicketController')->names('api.sourceTickets');
+  Route::apiResource('/profiles', 'ProfileController')->names('api.profiles');
 
   Route::get('/activity-course-registered-users/{activity_course_registered_user}', 'ActivityCourseRegisteredUserController@show')->name('api.activityCourseRegisteredUsers.show');
 
@@ -116,6 +117,7 @@ Route::group([
   Route::get('/classrooms/{classroom}/tickets', 'ClassroomController@tickets')->name('api.classrooms.courseRegisteredUsers');
   Route::get('/roles/{role}/users', 'RoleController@users')->name('api.roles.users');
   Route::get('/source-tickets/{source_ticket}/tickets', 'SourceTicketController@tickets')->name('api.sourceTickets.tickets');
+  Route::get('/profiles/{profile}/course-registered-users', 'ProfileController@courseRegisteredUsers')->name('api.profiles.courseRegisteredUsers');
 
   Route::get('/courses/{course}/activities', 'CourseController@activities')->name('api.courses.activities');
   Route::get('/courses/{course}/registered-users', 'CourseController@registeredUsers')->name('api.courses.registeredUsers');
