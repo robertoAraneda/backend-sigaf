3cc7f1d593da8688f03fc153bacf7a80f163d1c7
Roberto Araneda Espinoza <robaraneda@gmail.com>
2021-03-07 20:22:31 -0300

    se agrega hora de creación a detalle de ticket y tickets

--- app/Http/Resources/Json/StatusDetailTicket.php
+++ app/Http/Resources/Json/StatusDetailTicket.php
@@ -7,20 +7,20 @@ use Carbon\Carbon;
 
 class StatusDetailTicket extends JsonResource
 {
-  /**
-   * Transform the resource into an array.
-   *
-   * @param  \Illuminate\Http\Request  $request
-   * @return array
-   */
-  public function toArray($request)
-  {
-    return [
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
       'links' => [
         'href' => route(
-          'api.statusDetailTickets.show',
-          ['status_detail_ticket' => $this->id],
-          false
+            'api.statusDetailTickets.show',
+            ['status_detail_ticket' => $this->id],
+            false
         ),
         'rel' => 'self'
       ],
@@ -28,12 +28,12 @@ class StatusDetailTicket extends JsonResource
         'id' => $this->id,
         'description' => $this->description,
         'createdAt' => $this->created_at != null
-          ?  Carbon::parse($this->created_at)->format('d-m-Y')
+          ?  Carbon::parse($this->created_at)->format('d-m-Y H:i:S')
           : null,
         'updatedAt' => $this->updated_at != null
           ?  Carbon::parse($this->updated_at)->format('d-m-Y')
           : null
       ]
     ];
-  }
+    }
 }
--- app/Models/StatusDetailTicket.php
+++ app/Models/StatusDetailTicket.php
@@ -7,25 +7,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class StatusDetailTicket extends Model
 {
-  protected $fillable = [
+    protected $fillable = [
     'description'
   ];
 
-  protected $table = 'status_detail_tickets';
+    protected $table = 'status_detail_tickets';
 
-  /**
-   * Get the Status Detail Ticket formated
-   *
-   * @return array
-   */
-  public function format()
-  {
-    return [
+    /**
+     * Get the Status Detail Ticket formated
+     *
+     * @return array
+     */
+    public function format()
+    {
+        return [
       'links' => [
         'href' => route(
-          'api.statusDetailTickets.show',
-          ['status_detail_ticket' => $this->id],
-          false
+            'api.statusDetailTickets.show',
+            ['status_detail_ticket' => $this->id],
+            false
         ),
         'rel' => 'self'
       ],
@@ -33,7 +33,7 @@ class StatusDetailTicket extends Model
         'id' => $this->id,
         'description' => $this->description,
         'createdAt' => $this->created_at != null
-          ?  Carbon::parse($this->created_at)->format('d-m-Y')
+          ?  Carbon::parse($this->created_at)->format('d-m-Y H:i:s')
           : null,
         'updatedAt' => $this->updated_at != null
           ?  Carbon::parse($this->updated_at)->format('d-m-Y')
@@ -43,22 +43,22 @@ class StatusDetailTicket extends Model
         'numberOfElements' => $this->ticketDetails->count(),
         'links' => [
           'href' => route(
-            'api.statusDetailTickets.ticketDetails',
-            ['status_detail_ticket' => $this->id],
-            false
+              'api.statusDetailTickets.ticketDetails',
+              ['status_detail_ticket' => $this->id],
+              false
           ),
           'rel' => '/rels/ticketDetails'
         ]
       ]
     ];
-  }
+    }
 
-  /**
-   * Get a list of ticket details for the status detail ticket
-   *
-   */
-  public function ticketDetails()
-  {
-    return $this->hasMany(TicketDetail::class);
-  }
+    /**
+     * Get a list of ticket details for the status detail ticket
+     *
+     */
+    public function ticketDetails()
+    {
+        return $this->hasMany(TicketDetail::class);
+    }
 }
