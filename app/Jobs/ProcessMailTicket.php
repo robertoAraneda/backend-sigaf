aa4dcc7dae6729c9484911d34a4e18c3e6189c6c
Roberto Araneda Espinoza <robaraneda@gmail.com>
2021-03-30 11:19:54 -0300

    se corrige error de cc

--- app/Jobs/ProcessMailTicket.php
+++ app/Jobs/ProcessMailTicket.php
@@ -92,7 +92,11 @@ class ProcessMailTicket implements ShouldQueue
         Mail::send('ticket', compact('data'), function ($message) use ($files, $primaryEmails, $ccEmails, $data) {
             $message->from($data['emailCourse']);
             $message->to($primaryEmails);
-            $message->cc($ccEmails);
+
+            if (isset($ccEmails)) {
+                $message->cc($ccEmails);
+            }
+          
             $message->subject($data['subject']);
 
             if (count($files) > 0) {
